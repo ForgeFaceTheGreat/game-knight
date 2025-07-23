ce871cd417de7b50f0ce76cd647bd4294b8ff76d
JamesCalebWay <jcway324@gmail.com>
2025-07-23 17:22:47 -0400

    first commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Repository Name
+var GHPath = '/Game Knight';
+
+// App prefix name
+var APP_PREFIX = 'GK';
+
+// ****Change version number any time any files are changed or else****
+var VERSION = 'version_0.0.0.0';
+
+var URLS = [
+    `${GHPath}/`,
+    `${GHPath}/index.html`,
+    `${GHPath}/style.css`
+    `${GHPath}/scripts/app.js`
+]
+
+// https://christianheilmann.com/2022/01/13/turning-a-github-page-into-a-progressive-web-app/
