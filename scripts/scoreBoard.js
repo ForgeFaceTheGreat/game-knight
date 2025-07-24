141cb6c8e9c57e23138bfec759556e21e2b1133b
JamesCalebWay <jcway324@gmail.com>
2025-07-24 18:19:14 -0400

    scoreboard add players etc

--- scripts/scoreBoard.js
+++ scripts/scoreBoard.js
@@ -1,2 +1,33 @@
-const players = ["John", "Hammy", "Stupid Slime", "Bert"];
+const players = [];
 
+class player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
+    }
+}
+
+// let playerName;
+
+document.getElementById("addButton").onclick = function() {
+    let playerName = document.getElementById("playerName").value;
+
+    // Construct a player
+    players.push({ name: playerName, score: 0 });
+
+    // Prints Player name to screen
+    // document.getElementById("playerList").innerHTML = "<li>" + players[0].name + "</li>";
+    printScoreBoard();
+
+    // Resets text box
+    document.getElementById("playerName").value = "";
+}
+
+function printScoreBoard() {
+    let string = "";
+
+    for (let i = 0; i < players.length; i++) {
+        string += "<tr>\n<td>" + players[i].name + "</td>\n<td>" + players[i].score + "</td>\n</tr>";
+    }
+    document.getElementById("playerList").innerHTML = string;
+}
